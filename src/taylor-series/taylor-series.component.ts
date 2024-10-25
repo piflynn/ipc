1f2b2384fe83a4194f9bc684bacfd1cae476da9c
Ian Flynn <piflynn@me.com>
2024-10-24 23:07:02 -0400

    updates

--- src/taylor-series/taylor-series.component.ts
+++ src/taylor-series/taylor-series.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+  ],
   selector: 'app-taylor-series',
   templateUrl: './taylor-series.component.html',
   styleUrl: './taylor-series.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TaylorSeriesComponent {}
+export class TaylorSeriesComponent {
+  estimatePi(input: string) {
+    const iterations = parseInt(input);
+    const results = [];
+    let sum = 0;
+    let i = 1;
+    let iteration = 1;
+    while (iteration <= iterations) {
+      const exp = (i - 1) / 2;
+
+      sum += Math.pow(-1, exp) * (Math.pow(1, i) / i);
+
+      const pi = 4 * sum;
+      const error = Math.abs(pi - Math.PI);
+      const percentError = (error / Math.PI) * 100;
+
+      results.push({ i, pi, error, percentError });
+      i += 2;
+      iteration++;
+    }
+    return results;
+  }
+}
--- src/viewer/viewer.component.ts
+++ src/viewer/viewer.component.ts
@@ -1,41 +1,58 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, OnInit, inject } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  DestroyRef,
+  OnInit,
+  inject,
+} from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { MatCardModule } from '@angular/material/card';
-import { catchError, from, map, mergeMap, of, switchMap, timer, toArray } from 'rxjs';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import {
+  catchError,
+  from,
+  map,
+  mergeMap,
+  of,
+  switchMap,
+  timer,
+  toArray,
+} from 'rxjs';
 import { DisplayImage, StreamBlob } from 'src/models';
 import { OrchidService } from 'src/orchid.service';
 @Component({
   selector: 'app-viewer',
   standalone: true,
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatProgressSpinnerModule, MatCardModule],
   templateUrl: './viewer.component.html',
   styleUrl: './viewer.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ViewerComponent implements OnInit {
   private readonly requestConcurrency = 2;
   private readonly destroyRef = inject(DestroyRef);
   private readonly pollingInterval$ = timer(0, 10 * 1000); // use timer to create 5 second polling interval
   images: DisplayImage[] = [];
+  loaded = false;
 
   constructor(private orchid: OrchidService, private cd: ChangeDetectorRef) {}
 
   ngOnInit() {
     this.orchid
-    .createUserSession()
-    .pipe(
-      // use polling to refetch
-      switchMap((session) =>
-        this.pollingInterval$.pipe(
-          switchMap(() => this.getPrimaryFrames(session.id))
-        )
-      ),
-      // stop polling on component destroy
-      takeUntilDestroyed(this.destroyRef)
-    )
-    .subscribe(
-      (frames) =>{
+      .createUserSession()
+      .pipe(
+        // use polling to refetch
+        switchMap((session) =>
+          this.pollingInterval$.pipe(
+            switchMap(() => this.getPrimaryFrames(session.id))
+          )
+        ),
+        // stop polling on component destroy
+        takeUntilDestroyed(this.destroyRef)
+      )
+      .subscribe((frames) => {
         this.images = frames
           .sort((a, b) => b.stream.id - a.stream.id)
           .map((frame) => ({
@@ -43,23 +60,22 @@ export class ViewerComponent implements OnInit {
             name: frame.stream.name,
             url: URL.createObjectURL(frame.blob),
           }));
+        this.loaded = true;
         this.cd.markForCheck();
-      }
-        
-    );
+      });
 
-  // timer(0, 5 * 1000)
-  //   .pipe(takeUntilDestroyed(this.destroyRef))
-  //   .subscribe(() => {
-  //     console.log('hi')
+    // timer(0, 5 * 1000)
+    //   .pipe(takeUntilDestroyed(this.destroyRef))
+    //   .subscribe(() => {
+    //     console.log('hi')
 
-  //     const timestamp = new Date().getTime();
-  //     this.images = this.images.map(image => ({...image, url: `${image.url}?t=${timestamp}`}));
-  //     console.log(this.images[0])
-  //   });
+    //     const timestamp = new Date().getTime();
+    //     this.images = this.images.map(image => ({...image, url: `${image.url}?t=${timestamp}`}));
+    //     console.log(this.images[0])
+    //   });
   }
 
-    /**
+  /**
    * Gets the frame image urls (as blobs) and stream data
    *
    * @remarks
@@ -72,23 +88,23 @@ export class ViewerComponent implements OnInit {
    * @returns an object containing the blob imageUrl from the frame and the associated stream
    *
    */
-    private getPrimaryFrames(sessionId: string) {
-      return this.orchid.getStreams(sessionId).pipe(
-        map((res) => res.streams),
-        switchMap((streams) =>
-          from(streams).pipe(
-            mergeMap(
-              (stream) =>
-                this.orchid.getFrame(sessionId, stream.id).pipe(
-                  map((blob) => ({ stream, blob } as StreamBlob)),
-                  // TODO: Add more robust error handling
-                  catchError((err) => of()) // prevents successive requests from getting canceled after a failure
-                ),
-              this.requestConcurrency
-            ),
-            toArray()
-          )
+  private getPrimaryFrames(sessionId: string) {
+    return this.orchid.getStreams(sessionId).pipe(
+      map((res) => res.streams),
+      switchMap((streams) =>
+        from(streams).pipe(
+          mergeMap(
+            (stream) =>
+              this.orchid.getFrame(sessionId, stream.id).pipe(
+                map((blob) => ({ stream, blob } as StreamBlob)),
+                // TODO: Add more robust error handling
+                catchError((err) => of()) // prevents successive requests from getting canceled after a failure
+              ),
+            this.requestConcurrency
+          ),
+          toArray()
         )
-      );
-    }
+      )
+    );
+  }
 }
